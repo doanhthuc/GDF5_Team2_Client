3f5e6e717315af8471bb6f55d1e79c99a19b2534
hovanvydut <hovanvydut@gmail.com>
2022-06-13 09:14:29 +0700

    sync code

--- src/modules/battle/GameLayer.js
+++ src/modules/battle/GameLayer.js
@@ -1,8 +1,21 @@
 let GameLayer = cc.Layer.extend({
     ctor: function () {
         this._super();
-        this.spriteFrameCache.load("texture/**");
-        let rootNode = ccs.load("ui/battle/BattleUILayer.json");
+        let rootNode = ccs.load("ui/battle/BattleScene.json", "");
         this.addChild(rootNode.node);
+        this.handleEvent();
+    },
+
+    handleEvent: function () {
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function (key, event) {
+                cc.log("Key down:" + key);
+                cc.log(this.getChildrenCount());
+            }.bind(this),
+            onKeyReleased: function (key, event) {
+                cc.log("Key up:" + key);
+            }.bind(this)
+        }, this);
     }
 });
