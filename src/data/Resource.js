a857e75f2a2c6385096d50a942b37c35822751c8
vyhv <hovanvydut@gmail.com>
2022-06-24 15:27:09 +0700

    update

--- src/data/Resource.js
+++ src/data/Resource.js
@@ -80,7 +80,7 @@ var res = {
     MAIN_SCREEN: 'ui/main/MainScene.json',
     // BOTTOM_NAV: 'ui/main/bottom/BottomNav.json',
     LOBBY_NODE: 'ui/lobby/lobbyHomeNode.json',
-    TREASURE_POPUP_NODE: 'ui/lobby/treasurePopup.json',
+    TREASURE_POPUP_NODE: 'ui/lobby/treasurePopUp.json',
 
 
     //popup
