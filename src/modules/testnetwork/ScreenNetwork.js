4c06fbf305899f7a9c51f6536ca1dd514d0b4c88
CPU60274\CPU60274-local <sondxm@vng.com.vn>
2022-07-02 15:51:35 +0700

    .

--- src/modules/testnetwork/ScreenNetwork.js
+++ src/modules/testnetwork/ScreenNetwork.js
@@ -13,93 +13,10 @@ var ScreenNetwork = cc.Layer.extend({
         this.btnLogin = this.loginNode.getChildByName("Button_1");
         this.btnLogin.addClickEventListener(this.onSelectLogin.bind(this));
         this.textFieldUID = this.loginNode.getChildByName("TextField_1");
-        // var yBtn = 2*size.height/3;
-        // this.nodeUI = new cc.Node();
-        //
-        // this.addChild(this.nodeUI);
-        // var btnLogin = gv.commonButton(200, 64, size.width/4, yBtn,"Login");
-        // this.nodeUI.addChild(btnLogin);
-        // btnLogin.addClickEventListener(this.onSelectLogin.bind(this));
-
-        // // var btnDisconnect = gv.commonButton(200, 64, size.width/2, yBtn,"Disconnect");
-        // this.nodeUI.addChild(btnDisconnect);
-        // btnDisconnect.addClickEventListener(this.onSelectDisconnect.bind(this));
-        //
-        // var btnReconnect = gv.commonButton(200, 64, 3*size.width/4, yBtn,"Reconnect");
-        // this.nodeUI.addChild(btnReconnect);
-        // btnReconnect.addClickEventListener(this.onSelectReconnect.bind(this));
-        //
         this.lblLog = gv.commonText(fr.Localization.text("..."), size.width * 0.5, size.height * 0.05);
         this.addChild(this.lblLog);
 
         //this.initGame();
-    },
-    initGame: function () {
-        var size = cc.director.getVisibleSize();
-
-        this._gameNode = new cc.Node();
-        this._gameNode.setPosition(cc.p(size.width * 0.5 - MAP_SIZE * TILE_SIZE / 2, size.height * 0.5 - MAP_SIZE * TILE_SIZE / 2));
-        this._gameNode.setVisible(false);
-        this.addChild(this._gameNode);
-
-        //
-        this.tileImgs = [];
-        for (var i = 0; i < MAP_SIZE; i++) {
-            this.tileImgs[i] = [];
-            for (var j = 0; j < MAP_SIZE; j++) {
-                var tileSprite = new cc.Sprite('res/game/tile.png');
-                tileSprite.setPosition(this.getPositionFromTilePos(i, j));
-                this._gameNode.addChild(tileSprite);
-                this.tileImgs[i][j] = tileSprite;
-            }
-        }
-        //
-        this.character = fr.createAnimationById(resAniId.chipu, this);
-        //doi mau, yeu cau phai co file shader, nhung bone co ten bat dau tu color_ se bi doi mau
-        this.character.setBaseColor(255, 0, 0);
-        //chinh toc do play animation
-        this.character.getAnimation().setTimeScale(0.5);
-        this._gameNode.addChild(this.character);
-        //play animation gotoAndPlay(animationName, fadeInTime, duration, playTimes)
-        this.character.getAnimation().gotoAndPlay("idle_0_", -1);
-
-        this.reset();
-
-        this._gameController = new cc.Node();
-        this._gameController.setPosition(cc.p(100, 100));
-        this._gameController.setVisible(false);
-        this.addChild(this._gameController);
-
-        var btnReset = gv.commonButton(128, 64, 0, 164, "Reset");
-        this._gameController.addChild(btnReset);
-        btnReset.addClickEventListener(function () {
-            testnetwork.connector.sendResetMap();
-        }.bind(this));
-
-        var btnLeft = gv.commonButton(64, 64, -64, 0, "<");
-        this._gameController.addChild(btnLeft);
-        btnLeft.addClickEventListener(function () {
-            this.sendMove(this.character.tileX - 1, this.character.tileY);
-        }.bind(this));
-
-        var btnRight = gv.commonButton(64, 64, 64, 0, ">");
-        this._gameController.addChild(btnRight);
-        btnRight.addClickEventListener(function () {
-            this.sendMove(this.character.tileX + 1, this.character.tileY);
-        }.bind(this));
-        var btnUp = gv.commonButton(64, 64, 0, 64, "<");
-        btnUp.setRotation(90);
-        this._gameController.addChild(btnUp);
-        btnUp.addClickEventListener(function () {
-            this.sendMove(this.character.tileX, this.character.tileY + 1);
-        }.bind(this));
-        var btnDown = gv.commonButton(64, 64, 0, -64, ">");
-        btnDown.setRotation(90);
-        this._gameController.addChild(btnDown);
-        btnDown.addClickEventListener(function () {
-            this.sendMove(this.character.tileX, this.character.tileY - 1);
-        }.bind(this));
-
     },
     reset: function () {
         for (var i = 0; i < MAP_SIZE; i++) {
