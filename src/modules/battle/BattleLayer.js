a4c5648672c452fcb1b6a8f83ef6b2b7aff0dd15
vyhv <hovanvydut@gmail.com>
2022-07-27 17:41:49 +0700

    update: config

--- src/modules/battle/BattleLayer.js
+++ src/modules/battle/BattleLayer.js
@@ -133,8 +133,8 @@ let BattleLayer = cc.Layer.extend({
         // }, 5000);
         EntityFactory.createAssassinMonster(pixelPos, mode);
         // EntityFactory.createGiantMonster(pixelPos, mode);
-        // EntityFactory.createSatyrBoss(pixelPos, mode);
-        // EntityFactory.createNinjaMonster(pixelPos, mode);
+        EntityFactory.createSatyrBoss(pixelPos, mode);
+        EntityFactory.createNinjaMonster(pixelPos, mode);
         // EntityFactory.createSwordsmanMonster(pixelPos, mode);
         // EntityFactory.createSwordsmanMonster(pixelPos, mode);
 
