c506428f448b5e2a4fea4f2d5616e03234e496bb
ThucPD <thucpd@vng.com.vn>
2022-07-28 10:02:22 +0700

    .

--- src/modules/battle/ecs/config/TowerConfig.js
+++ src/modules/battle/ecs/config/TowerConfig.js
@@ -1,6 +1,6 @@
 let TowerConfig = TowerConfig || {};
 
-TowerConfig.getTowerConfigFromJson = function (towerId, level) {
+TowerConfig.getTowerConfigFromJson =  (towerId, level) => {
     let towerRank = ReadConfigUtils.getTowerRankByLevel(level);
     let towerConfig = JsonReader.getTowerConfig()[towerId];
     let cloneConfig = _.clone(towerConfig);
@@ -12,27 +12,35 @@ TowerConfig.getBunnyOilGunTowerConfigFromJson = (towerId, level) => {
     let towerRank = ReadConfigUtils.getTowerRankByLevel(level);
     let towerConfig = TowerConfig.getTowerConfigFromJson(towerId, level);
     let targetBuffConfigData = JsonReader.getTargetBuffConfig()[towerConfig.bulletTargetBuffType]
-    towerConfig.slowDuration = targetBuffConfigData.duration[towerRank];
-    towerConfig.slowValue = targetBuffConfigData.effects[towerRank][0].value;
+    let cloneConfig = _.clone(targetBuffConfigData);
+    cloneConfig.slowDuration = targetBuffConfigData.duration[towerRank];
+    cloneConfig.slowValue = targetBuffConfigData.effects[towerRank][0].value;
+    return cloneConfig;
 }
 
 TowerConfig.getBearIceGunTowerConfigFromJson = (towerId, level) => {
     let towerRank = ReadConfigUtils.getTowerRankByLevel(level);
     let towerConfig = TowerConfig.getTowerConfigFromJson(towerId, level);
     let targetBuffConfigData = JsonReader.getTargetBuffConfig()[towerConfig.bulletTargetBuffType]
-    towerConfig.frozenDuration = targetBuffConfigData.duration[towerRank];
+    let cloneConfig = _.clone(targetBuffConfigData);
+    cloneConfig.frozenDuration = targetBuffConfigData.duration[towerRank];
+    return cloneConfig;
 }
 
 TowerConfig.getDamageGoatTowerConfigFromJson = (towerId, level) => {
     let towerRank = ReadConfigUtils.getTowerRankByLevel(level);
     let towerConfig = TowerConfig.getTowerConfigFromJson(towerId, level);
     let towerBuffConfigData = JsonReader.getTowerBuffConfig()[towerConfig.auraTowerBuffType]
-    towerConfig.damageUpValue = towerBuffConfigData.effects[towerRank][0].value;
+    let cloneConfig = _.clone(towerBuffConfigData);
+    cloneConfig.damageUpValue = towerBuffConfigData.effects[towerRank][0].value;
+    return cloneConfig;
 }
 
 TowerConfig.getAttackSpeedSnakeTowerConfigFromJson = (towerId, level) => {
     let towerRank = ReadConfigUtils.getTowerRankByLevel(level);
     let towerConfig = TowerConfig.getTowerConfigFromJson(towerId, level);
     let towerBuffConfigData = JsonReader.getTowerBuffConfig()[towerConfig.auraTowerBuffType]
-    towerConfig.attackSpeedUpValue = towerBuffConfigData.effects[towerRank][0].value;
+    let cloneConfig = _.clone(towerBuffConfigData);
+    cloneConfig.attackSpeedUpValue = towerBuffConfigData.effects[towerRank][0].value;
+    return cloneConfig;
 }
