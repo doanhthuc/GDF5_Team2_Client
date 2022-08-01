309629ffaffa482bd8121e6cf5dbdb11ef3a86f3
Prime <doanhthucdn3005@gmail.com>
2022-08-01 23:14:07 +0700

    select card battle deck in inventory FE

--- src/modules/inventory/InventoryLayer.js
+++ src/modules/inventory/InventoryLayer.js
@@ -32,6 +32,17 @@ const InventoryLayer = cc.Node.extend({
         this.fakeCardImageFromCardCollection = new CardNode();
         this.addChild(this.fakeCardImageFromCardCollection);
         this.fakeCardImageFromCardCollection.setVisible(false);
+
+        let cardSize = this.fakeCardImageFromCardCollection.cardBorderImg.getContentSize();
+        let posY = cc.winSize.height - (this.headerHeight + this.battleDeckNode.heightNode + cardSize.height / 2 + 10);
+        this.fakeCardImageFromCardCollection.setPosition(cc.winSize.width / 2, posY);
+
+        this.cancelSelectBtn = new ccui.Button();
+        this.cancelSelectBtn.loadTextures("res/textures/common/common_panel_btn_close.png", "res/textures/common/common_panel_btn_close.png");
+        this.addChild(this.cancelSelectBtn);
+        this.cancelSelectBtn.setVisible(false);
+        this.cancelSelectBtn.setPosition(this.fakeCardImageFromCardCollection.x, posY - (cardSize.height / 2 + 20));
+        this.cancelSelectBtn.addTouchEventListener(this.onCancelSelectCardBtnClick, this);
     },
 
     initCardNodeList: function () {
@@ -51,39 +62,55 @@ const InventoryLayer = cc.Node.extend({
         this.fakeCardImageFromCardCollection.setModel(cardModel);
         this.fakeCardImageFromCardCollection.setUpgradeProgressBarVisible(false);
         this.fakeCardImageFromCardCollection.setVisible(true);
+        this.cancelSelectBtn.setVisible(true);
         this.isSelectingCardToBattleDeck = true;
         this.cardCollectionNode.setVisible(false);
         let cardSize = this.fakeCardImageFromCardCollection.cardBorderImg.getContentSize();
         let posY = cc.winSize.height - (this.headerHeight + this.battleDeckNode.heightNode + cardSize.height / 2 + 10);
-        this.fakeCardImageFromCardCollection.setPosition(cc.winSize.width / 2 , posY);
+        this.fakeCardImageFromCardCollection.setPosition(cc.winSize.width / 2, posY);
     },
 
-    onSelectCardInBattleDeckToSwap: function (cardModel) {
-        let cardNode = this.cardNodeMap.get(cardModel.id);
-        let wordPos = cardNode.getParent().convertToWorldSpace(cardNode.getPosition());
-        this.fakeCardImageFromBattleDeck.setModel(cardModel);
+    onSelectCardInBattleDeckToSwap: function (cardModelFromBattleDeck) {
+        let cardNodeInBattleDeck = this.cardNodeMap.get(cardModelFromBattleDeck.id);
+        let wordPos = cardNodeInBattleDeck.getParent().convertToWorldSpace(cardNodeInBattleDeck.getPosition());
+        this.fakeCardImageFromBattleDeck.setModel(cardModelFromBattleDeck);
         this.fakeCardImageFromBattleDeck.setUpgradeProgressBarVisible(false);
+        this.cancelSelectBtn.setVisible(false);
         this.fakeCardImageFromBattleDeck.setVisible(true);
         this.fakeCardImageFromBattleDeck.setPosition(wordPos);
         this.isSelectingCardToBattleDeck = false;
-        let cardInCollection = this.cardNodeMap.get(this.fakeCardImageFromCardCollection.cardModel.id);
+        let cardModelFromCardCollection = this.fakeCardImageFromCardCollection.cardModel;
+        let cardInCollection = this.cardNodeMap.get(cardModelFromCardCollection.id);
         let cardInCollectionWordPos = cardInCollection.getParent().convertToWorldSpace(cardInCollection.getPosition());
         this.cardCollectionNode.setVisible(true);
         this.fakeCardImageFromCardCollection.setPosition(cardInCollectionWordPos);
-        cardNode.setVisible(false);
+        cardNodeInBattleDeck.setVisible(false);
         cardInCollection.setVisible(false);
-        this.fakeCardImageFromBattleDeck.runAction(cc.Sequence(cc.moveTo(1, cardInCollectionWordPos), cc.CallFunc(function () {
+        this.fakeCardImageFromBattleDeck.runAction(cc.Sequence(cc.moveTo(0.3, cardInCollectionWordPos), cc.CallFunc(function () {
             this.fakeCardImageFromBattleDeck.setVisible(false);
-            cardNode.setModel(this.fakeCardImageFromCardCollection.cardModel);
-            cardNode.setVisible(true);
+            cardNodeInBattleDeck.setModel(cardModelFromCardCollection);
+            cardNodeInBattleDeck.setVisible(true);
         }.bind(this))));
         // this.fakeCardImageFromCardCollection.runAction(cc.moveTo(1, wordPos));
-        this.fakeCardImageFromCardCollection.runAction(cc.Sequence(cc.moveTo(1, wordPos), cc.CallFunc(function () {
+        this.fakeCardImageFromCardCollection.runAction(cc.Sequence(cc.moveTo(0.3, wordPos), cc.CallFunc(function () {
             this.fakeCardImageFromCardCollection.setVisible(false);
-            cardInCollection.setModel(this.fakeCardImageFromBattleDeck.cardModel);
+            cardInCollection.setModel(cardModelFromBattleDeck);
             cardInCollection.setVisible(true);
         }.bind(this))));
+        cardModelFromCardCollection.isBattleDeck = true;
+        cardModelFromBattleDeck.isBattleDeck = false;
+        ClientUIManager.getInstance().getUI(CLIENT_UI_CONST.NODE_NAME.INVENTORY_NODE).cardNodeMap.set(cardModelFromCardCollection.id, cardNodeInBattleDeck);
+        ClientUIManager.getInstance().getUI(CLIENT_UI_CONST.NODE_NAME.INVENTORY_NODE).cardNodeMap.set(cardModelFromBattleDeck.id, cardInCollection);
         // this.fakeCardImageFromBattleDeck.setVisible(false);
         // this.fakeCardImageFromCardCollection.setVisible(false);
+    },
+
+    onCancelSelectCardBtnClick: function (sender, type) {
+        if (type === ccui.Widget.TOUCH_ENDED) {
+            this.isSelectingCardToBattleDeck = false;
+            this.cardCollectionNode.setVisible(true);
+            this.fakeCardImageFromCardCollection.setVisible(false);
+            this.cancelSelectBtn.setVisible(false);
+        }
     }
 });
