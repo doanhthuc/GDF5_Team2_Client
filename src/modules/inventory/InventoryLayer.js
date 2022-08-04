f4e523420a9be344d12f436c33a9b102702aca98
Prime <doanhthucdn3005@gmail.com>
2022-08-04 22:13:53 +0700

    shaking card while selecting card into battle deck in inventory

--- src/modules/inventory/InventoryLayer.js
+++ src/modules/inventory/InventoryLayer.js
@@ -71,6 +71,8 @@ const InventoryLayer = cc.Node.extend({
         let cardSize = this.fakeCardImageFromCardCollection.cardBorderImg.getContentSize();
         let posY = cc.winSize.height - (this.headerHeight + this.battleDeckNode.heightNode + cardSize.height / 2 + 10);
         this.fakeCardImageFromCardCollection.setPosition(cc.winSize.width / 2, posY);
+        this.makeAllCardNodeShaking();
+        this.makeCarNodeShaking(this.fakeCardImageFromCardCollection);
     },
 
     onSelectCardInBattleDeckToSwap: function (cardModelFromBattleDeck) {
@@ -89,6 +91,8 @@ const InventoryLayer = cc.Node.extend({
         this.fakeCardImageFromCardCollection.setPosition(cardInCollectionWordPos);
         cardNodeInBattleDeck.setVisible(false);
         cardInCollection.setVisible(false);
+        this.stopAllCardNodeShaking();
+        this.stopCarNodeShaking(this.fakeCardImageFromCardCollection);
         this.fakeCardImageFromBattleDeck.runAction(cc.Sequence(cc.moveTo(0.3, cardInCollectionWordPos), cc.CallFunc(function () {
             this.fakeCardImageFromBattleDeck.setVisible(false);
             cardNodeInBattleDeck.setModel(cardModelFromCardCollection);
@@ -127,5 +131,28 @@ const InventoryLayer = cc.Node.extend({
                 this.cancelSelectBtn.setVisible(false);
             }
         }
+    },
+
+    makeAllCardNodeShaking: function () {
+        for (let cardNode of this.cardNodeMap.values()) {
+            this.makeCarNodeShaking(cardNode);
+        }
+    },
+
+    stopAllCardNodeShaking: function () {
+        for (let cardNode of this.cardNodeMap.values()) {
+            this.stopCarNodeShaking(cardNode);
+        }
+    },
+
+    makeCarNodeShaking: function (cardNode) {
+        let rotateLeft = cc.RotateTo(0.2, -2, -2);
+        let rotateRight = cc.RotateTo(0.4, 4, 4);
+        cardNode.runAction(cc.Sequence(rotateLeft, rotateRight).repeatForever())
+    },
+
+    stopCarNodeShaking: function (cardNode) {
+        cardNode.stopAllActions();
+        cardNode.setRotation(0);
     }
 });
